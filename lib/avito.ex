2056f40cea7af6fa635a80740caaf3753481a948
Malo Skrylevo <majioa@yandex.ru>
2018-02-26 14:50:44 +0300

    initial commit

---
+++ lib/avito.ex
@@ -0,0 +1,18 @@
+defmodule Avito do
+  @moduledoc """
+  Documentation for Avito.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Avito.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/avito_test.exs
@@ -0,0 +1,8 @@
+defmodule AvitoTest do
+  use ExUnit.Case
+  doctest Avito
+
+  test "greets the world" do
+    assert Avito.hello() == :world
+  end
+end
